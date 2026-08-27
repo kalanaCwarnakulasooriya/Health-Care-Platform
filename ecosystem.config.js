f871eb34c5e5196c271a5eb10457e25c4ed74585
Dilini B Warnakulasooriya <dilinibhagya53@gmail.com>
2026-08-27 20:10:56 +0530

    eco file changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
     {
       name: "config-server",
       script: "java",
-      args: "-jar ./config-server/target/Config-Server-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar ./config-server/target/Config-Server-1.0.0-SNAPSHOT.jar --spring.profiles.active=dev",
       log_file:"./logs/config-server.log"
     },
     {
       name: "service-registry",
       script: "java",
-      args: "-jar ./service-registry/target/Service-Registry-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar ./service-registry/target/Service-Registry-1.0.0-SNAPSHOT.jar --spring.profiles.active=dev",
       log_file:"./logs/service-registry.log"
     },
     {
       name: "api-gateway",
       script: "java",
-      args: "-jar ./api-gateway/target/Api-Gateway-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar ./api-gateway/target/Api-Gateway-1.0.0-SNAPSHOT.jar --spring.profiles.active=dev",
       log_file:"./logs/api-gateway.log"
     }
   ]
